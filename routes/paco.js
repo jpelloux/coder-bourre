868194be3e42c5c83efa572bcd88c6f619821c05
Champey <champeymontange@gmail.com>
2020-04-08 15:52:29 +0200

    Server can react when a player press "menteur" or "toutpile"

--- routes/paco.js
+++ routes/paco.js
@@ -6,7 +6,8 @@ var io = null;
 var gameInfos = {
     "players": [],
     "nbReadyPlayers": 0,
-    "turn": 0
+    "turn": 0,
+    "dices": [0, 0, 0, 0, 0, 0] // total of each dice value during a round
 };
 
 function main(io){
@@ -41,22 +42,64 @@ function main(io){
             nsp.emit('startGame_res', '');
         });
 
-        socket.on('getDices', function(m){
-            socket.emit('getDices', getDices(5));
+        socket.on('getDices_req', function(m){
+            socket.emit('getDices_res', getDices(5));
             gameInfos.nbReadyPlayers++;
             if (gameInfos.nbReadyPlayers == gameInfos.players.length){
                 gameInfos.turn = Math.floor(Math.random() * gameInfos.players.length);
-                nsp.emit('startGame', gameInfos.players[gameInfos.turn]);
+                nsp.emit('startGame_res', gameInfos.players[gameInfos.turn]);
             }
         });
-    
-        socket.on('callMade', function(m){
+
+
+        /** "call" is an object which contains : 
+         *      - "pseudo" : the pseudo of the last player
+         *      - "numberCalled" : the last number of value called
+         *      - "valueCalled" : the last value called
+        */
+        socket.on('callMade_req', function(call){
             gameInfos.turn += 1;
             gameInfos.turn %= gameInfos.players.length;
-            var call = m[1] + '_' + m[2]
-            var disp = m[0] + ' annonce ' + m[1] + ' ' + m[2];
-            nsp.emit('callMade', {"toDisp": disp, "call": call, "player":gameInfos.players[gameInfos.turn]});
+            nsp.emit('callMade_res', {  "lastPlayer": call.pseudo,
+                                        "nextPlayer": gameInfos.players[gameInfos.turn], 
+                                        "numberCalled": call.numberCalled, 
+                                        "valueCalled":call.valueCalled });
         });
+
+        /** "data" is an object which contains :
+         *      - "pseudo": the pseudo of the player who ended the game
+         *      - "lastPlayer": the pseudo of the player before the one who ended the game
+         *      - "enderType": either "menteurButton" or "toutpileButton"
+         *      - "numberCalled": the last number of value called
+         *      - "valueCalled": the last value called
+        */
+        socket.on('endRound_req', function(data){
+            var hasWin;
+            var pseudo;
+
+            var valueCalled = parseInt(data.valueCalled);
+            var numberCalled = parseInt(data.numberCalled);
+
+            if(data.enderType=="menteurButton"){
+                hasWin = false;
+                pseudo = gameInfos.dices[valueCalled-1]+gameInfos.dices[0]<numberCalled ? data.lastPlayer : data.pseudo; //if the sum of the actual number of values called and pacos is less than the call, the last player loose
+            }else if(data.enderType=="toutpileButton"){
+                hasWin = gameInfos.dices[valueCalled-1]+gameInfos.dices[0]==numberCalled;
+                pseudo = data.pseudo;
+            }else{
+                throw "id of enderButton is not recognized";
+            }
+
+            nsp.emit('endRound_res', {
+                "endingPlayer":data.pseudo,
+                "enderType": data.enderType,
+                "hasWin": hasWin, 
+                "pseudo": pseudo, 
+                "dices": gameInfos.dices
+            });
+        });
+
+
     })
 
     router.get('/home', function(req, res){  
@@ -72,9 +115,30 @@ function main(io){
 function getDices(nbDices){
     var values = [];
         for (var j=0; j<nbDices; j++){
-            values.push(Math.floor(Math.random() * Math.floor(6)) + 1);
+            var dice = Math.floor(Math.random() * Math.floor(6)) + 1;
+            /*switch(dice){
+                case 1: gameInfos.dices.one++;break;
+                case 2: gameInfos.dices.two++;break;
+                case 3: gameInfos.dices.three++;break;
+                case 4: gameInfos.dices.four++;break;
+                case 5: gameInfos.dices.five++;break;
+                case 6: gameInfos.dices.six++;break;
+            }*/
+            gameInfos.dices[dice-1]++;
+            values.push(dice);
         }
     return values;
+}
+
+function getNbDice(value){
+    switch(value){
+        case 1: return gameInfos.dices.one;
+        case 2: return gameInfos.dices.two;
+        case 3: return gameInfos.dices.three;
+        case 4: return gameInfos.dices.four;
+        case 5: return gameInfos.dices.five;
+        case 6: return gameInfos.dices.six;
+    }
 }
 
 module.exports = main;
--- static/paco/paco_game.js
+++ static/paco/paco_game.js
@@ -6,12 +6,16 @@ var maxCallDisplayed = 10;
 
 
 
-// disp players list when I reach the game room
+/********************************************************************************************
+ * THE FOLLOWING SOCKETS AND FONCTIONS MANAGE THE CONNECTION OF PLAYERS AND START THE GAME  *
+*********************************************************************************************/
+
+// Displays players list when I reach the game room
 socket.emit("gameReached", pseudo, function(players){
     dispPlayersNames(players);
 });
 
-// update players list when a new player reach the game room
+// updates players list when a new player reach the game room
 socket.on("updatePlayersList", function(players){
     dispPlayersNames(players)
 });
@@ -25,6 +29,7 @@ function dispPlayersNames(namesTab){
     document.getElementById('playersName').innerHTML= "Joueurs présents: " + str;
 }
 
+// Displays and starts the game when someone press the starter button
 $('#start_button').click(function(){
     socket.emit("startGame_req", '');
 });
@@ -37,56 +42,63 @@ socket.on("startGame_res", function(){
 
 
 
+
+
+
+
+/**************************************************************************
+ * THE FOLLOWING SOCKETS AND FONCTIONS MANAGE THE PHASE OF GETTING DICES  *
+***************************************************************************/
+
 $('#getDices_button').click(function(){
-    socket.emit('getDices', '');
+    socket.emit('getDices_req', '');
     $('#getDices_button').prop('disabled', true);
 });
 
-socket.on('getDices', function(m){
+// Receives the values of dices from the server and displays the right images 
+socket.on('getDices_res', function(m){
     m.forEach(el => {
         $('#dices').append('<img src=\"/static/images/dice_' + el + '.png\">' + '\t');
     });
 });
 
-socket.on('startGame', function(player){
+
+
+
+
+
+
+
+/**************************************************************************************
+ * THE FOLLOWING SOCKETS AND FONCTIONS MANAGE THE CALLING SYSTEM (HEART OF THE GAME)  *
+***************************************************************************************/
+
+socket.on('startGame_res', function(player){
     roundManager(player, '')
 });
 
-socket.on('callMade', function(m){
-    $('#othersCalls').append('<p>' + m.toDisp + '</p>');
-    roundManager(m.player, m.call)
+/**
+ * Gets the information from the server that a call was made
+ * "data" is an object which contains :
+ *      - "lastPlayer" : the pseudo of the last player
+ *      - "nextPlayer" : the pseudo of the next player
+ *      - "numberCalled" : the last number of value called
+ *      - "valueCalled" : the last value called
+ */
+socket.on('callMade_res', function(data){
+    $('#othersCalls').append('<p>' + data.lastPlayer + ' annonce ' + data.numberCalled + ' ' + data.valueCalled + '</p>');
+    var call = data.numberCalled + '_' + data.valueCalled;
+    dispEndersButtons(data.lastPlayer, data.nextPlayer, call);
+    roundManager(data.nextPlayer, call);
 });
-
-
-
-
 
 function roundManager(player, call){
     document.getElementById('whosTurn').innerHTML = 'C\'EST AU TOUR DE '+ player.toUpperCase();
-    dispButtons(player);
     dispPossibleCalls(player, call);
-
 }
 
-function dispButtons(player){
-    if(player != pseudo){
-        document.getElementById('buttons').innerHTML = '';
-        return;
-    }
-    var values = '<p>';
-    values += '<button id="menteurButton" onClick="callButtonPressed(this.id)">MENTEUR !</button>';
-    values += '<p></p>';
-    values += '<button id="toutpileButton" onClick="callButtonPressed(this.id)">TOUT PILE</button>';
-    values += '</p>';
-
-    document.getElementById('buttons').innerHTML = values;
-}
-
-/* 
- * Génère le code html qui permet de faire une annonce
- *
- * call est sous la forme X_Y
-**/
+// Generates the html code that allows to make a call
+// "call" is in the form "X_Y"
 function dispPossibleCalls(player, call){
     if(player != pseudo){
         document.getElementById('numberBet').innerHTML = '';
@@ -100,22 +112,22 @@ function dispPossibleCalls(player, call){
         values += '<p>'
         for (var j=1; j<=maxCallDisplayed; j++){
             if(call == '') 
-            {// cas de la première annonce de la manche
-                if(i!=1){
-                    values += '<span onClick="reply_click(this.id)" id=\"value_' + i.toString() + '_' +  j.toString() + '\">';
+            {// case of the first call of the round
+                if(i!=1){ // we cannot start with a call on pacos
+                    values += '<span onClick="valuePressed(this.id)" id=\"value_' + i.toString() + '_' +  j.toString() + '\">';
                 }
                 else{
                     values += '<span style="color:#C4C4C4" id=\"value_' + j.toString() + '_' +  i.toString() + '\">';
                 }
             }
             else 
-            {//annonces suivantes
-                if(     (valueCalled!=1 && j==numberCalled && i>valueCalled) //permet les annonces de type X+_Y
-                        || (j>numberCalled && i==valueCalled) //permet les annonces de type X_Y+
-                        || (valueCalled!=1 && i==1 && j>=numberCalled/2) //permet des annonces en passage au paco
-                        || (valueCalled==1 && (i==1 && j>numberCalled || i!=1 && j>numberCalled*2)) //gère les cas où la dernière annonce était un paco
+            {// cases of the next calls
+                if(     (valueCalled!=1 && j==numberCalled && i>valueCalled) //allows the calls of type X+_Y
+                        || (j>numberCalled && i==valueCalled) //allows the calls of type X_Y+
+                        || (valueCalled!=1 && i==1 && j>=numberCalled/2) //allows the calls of pacos
+                        || (valueCalled==1 && (i==1 && j>numberCalled || i!=1 && j>numberCalled*2)) //manages the case where the last call was on pacos
                         ){
-                    values += '<span onClick="reply_click(this.id)" id=\"value_' + i.toString() + '_' +  j.toString() + '\">';
+                    values += '<span onClick="valuePressed(this.id)" id=\"value_' + i.toString() + '_' +  j.toString() + '\">';
                 }
                 else{
                     values += '<span style="color:#C4C4C4" id=\"value_' + j.toString() + '_' +  i.toString() + '\">';
@@ -132,9 +144,66 @@ function dispPossibleCalls(player, call){
 }
 
 
-function reply_click(id){
-    socket.emit('callMade', [pseudo, id.toString().split("_")[2], id.toString().split("_")[1]]);
+function valuePressed(id){
+    socket.emit('callMade_req', {"pseudo":pseudo, "numberCalled":id.toString().split("_")[2], "valueCalled":id.toString().split("_")[1] });
 }
 
-function callButtonPressed(id){
+
+
+
+
+
+
+/****************************************************************************************************
+ * THE FOLLOWING SOCKETS AND FONCTIONS MANAGE THE PRESSURE ON AN ENDER BUTTON (MENTEUR & TOUTPILE)  *
+*****************************************************************************************************/
+
+function dispEndersButtons(lastPlayer, player, call){
+    if(player != pseudo){
+        document.getElementById('enders_buttons').innerHTML = '';
+        return;
+    }
+    var values = '<p>';
+    values += '<button id="menteurButton" onClick="enderButtonPressed(this.id, \'' + call + '\', \'' + lastPlayer + '\')">MENTEUR !</button>';
+    values += '<p></p>';
+    values += '<button id="toutpileButton" onClick="enderButtonPressed(this.id, \'' + call + '\', \'' + lastPlayer + '\')">TOUT PILE</button>';
+    values += '</p>';
+    document.getElementById('enders_buttons').innerHTML = values;
 }
+
+function enderButtonPressed(enderType, lastCall, lastPlayer){
+    socket.emit('endRound_req', {
+        "pseudo": pseudo,
+        "lastPlayer": lastPlayer,
+        "enderType": enderType, 
+        "numberCalled":lastCall.toString().split("_")[0], 
+        "valueCalled":lastCall.toString().split("_")[1]})
+}
+
+/** "data" is an object which contains :
+ *      - "endingPlayer": the pseudo of the player who ended the game
+ *      - "enderType": either "menteurButton" or "toutpileButton"
+ *      - "hasWin": a boolean true if a dice is won, or truf if a dice is lost
+ *      - "pseudo": the pseudo of the player who have lose or win a dice
+ *      - "dices": an array containing the actual number of each dice (total of all hands)
+*/
+socket.on('endRound_res',function(data){
+    
+    //display who ended the game and how
+    var enderType = data.enderType=="menteurButton" ? "menteur !" : "tout pile !";
+    $('#othersCalls').append('<p>' + data.endingPlayer + ' annonce ' + enderType + '</p>');
+    
+    //display the number total of each value
+    var str = ["paco(s)", "deux", "trois", "quatre", "cinq", "six"];
+    var totalDicesStr = '<p style="text-align:left">Il y avait :<br>';
+    totalDicesStr    += '<ul style="text-align:left">';
+    for (var i=0; i<6; i++){
+        totalDicesStr+= '<li>' + data.dices[i] + ' ' + str[i] + '</li>';
+    }
+    totalDicesStr    += '</ul></p>';
+    $('#othersCalls').append(totalDicesStr);
+
+    //display who will loose or win a dice
+    var res = data.hasWin?"gagne":"perd";
+    $('#othersCalls').append('<p>' + data.pseudo + ' ' + res + ' ' + "un dé !" + '</p>');
+})
