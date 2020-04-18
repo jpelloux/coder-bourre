1169383226f79549c4d6b76b66f5799be24e5503
balkao <balkao12@gmail.com>
2020-04-18 20:02:21 +0200

    Essayez de peter ça bande de cons! Co-authored-by: Karabof <chevrot.lucas@gmail.com>

--- bots/bots.py
+++ bots/bots.py
@@ -4,11 +4,16 @@ import time
 import asyncio
 import os,random
 import sys
+import traceback
 
 class Bot(discord.Client):
 	tabWord={'cheh','honteux','kaamelott','suce','baise','wizz','vinjo', '_con', 'picole', 'monique'}
+	
 	def __init__(self):
 		super().__init__()
+		self.queue = []
+		self.queue_running = False
+		self.voice_client= None
 		
 
 	async def on_ready(self):
@@ -20,55 +25,74 @@ class Bot(discord.Client):
 	async def on_message(self, message):
 		
 		
+		words=[ x for x in self.tabWord if x in message.content.lower() ]
 		if (message.author==self.user):
 			return
 		try:
-			words=[ x for x in self.tabWord if x in message.content.lower() ]
 			if words:
 				user=message.author
 				voice_channel=user.voice.channel
-				channel=None
 				if voice_channel!= None:
-					channel=voice_channel.name
-					try:
-						vc= await voice_channel.connect()
-					except discord.errors.ClientException as ec:
-						log(ec)
-
+					# if not voice_channel.name in self.queue:
+					# 	print('Create queue for ' + voice_channel.name)
+					# 	self.queue[voice_channel.name] = []
 					for i in words:
-						if i == 'kaamelott':
-								path='sounds/kaamelott/'
-								randomFile=random.choice(os.listdir(path))
-								player = vc.play(discord.FFmpegPCMAudio(path+randomFile), after=lambda e: print('done', e))
-								while vc.is_playing():
-									await asyncio.sleep(1)
-								vc.stop()
+						print('Add queue for ' + voice_channel.name + '   ' + i)
+						self.queue.append((i, message.channel, voice_channel))
+
+					if self.queue_running:
+						return
+
+					while len(self.queue) > 0:
+
+						print("While queue length left : " + str(len(self.queue)))
+						self.queue_running = True
+						try:
+							self.voice_client = await self.queue[0][2].connect()
+						except discord.errors.ClientException as ec:
+							log(ec)
+
+						print("before play sound")
+						await self.play_sound()
+						print("after play sound")
+						if len(self.queue) > 0 and self.queue[0][2].name != self.voice_client.channel.name:
+							await self.voice_client.disconnect()
 						else:
-							if os.path.isfile("gifs/"+i+".gif"):
-								await message.channel.send(file=discord.File("gifs/"+i+".gif"))
-								
-							player = vc.play(discord.FFmpegPCMAudio('sounds/'+i+'.mp3'), after=lambda e: print('done', e))
-							while vc.is_playing():
-								await asyncio.sleep(1)
-							vc.stop()
-					await message.channel.send('fin du for')
-					await vc.disconnect()
+							print('je me deco pas')	
+							
+					await self.voice_client.disconnect()
+					self.queue_running = False
+
 				else:
-					await bot.say('User is not in a channel.')
+					await message.channel.send('t\'es pas dans un channel blaireau!')
 		except Exception as e :
 			log(e)
-			
+			log(traceback.print_exc())
 
-	
+	async def play_sound(self):
+		sound, channel,voice_channel = self.queue.pop(0)
+
+		
+		if sound == 'kaamelott':
+			path='sounds/kaamelott/'
+			randomFile=random.choice(os.listdir(path))
+			self.voice_client.play(discord.FFmpegPCMAudio(path+randomFile), after=lambda e: print('done', e))
+			while self.voice_client.is_playing():
+				await asyncio.sleep(1)
+			self.voice_client.stop()
+		else:
+			if os.path.isfile("gifs/"+sound+".gif"):
+				await channel.send(file=discord.File("gifs/"+sound+".gif"))
+				
+			self.voice_client.play(discord.FFmpegPCMAudio('sounds/'+sound+'.mp3'), after=lambda e: print('done', e))
+			while self.voice_client.is_playing():
+				await asyncio.sleep(1)
+			self.voice_client.stop()
+		
+
 def log(e):
 	print('erreur service',e)
 	sys.stdout.flush()
-
-
-
-
-
-
 
 if __name__=="__main__":
 	bot=Bot()
